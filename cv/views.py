927d1875a9959df76201822663a79e5e23cc36b8
Arthur Rio <arthurio8@hotmail.com>
2011-09-07 19:34:15 -0700

    changement d'ordinateur

--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.conf import settings 
+
+import json
 
 def cv(request):
-    template_context = {}
+    common = json.load(open('%s/data/common.json' % (settings.STATIC_REL_URL),'r'))
+    if request.META.get('LANG') and request.META.get('LANG').count('fr') != 0:
+        data = json.load(open('%s/data/fr.json' % (settings.STATIC_REL_URL),'r'))
+    else:
+        data = json.load(open('%s/data/en.json' % (settings.STATIC_REL_URL),'r'))
+
+    template_context = {'common':common,'data':data}
+
     return render_to_response('index.html', template_context, RequestContext(request))
--- static/js/script.js
+++ static/js/script.js
@@ -10,16 +10,51 @@
 //Begin Core PunchTab Javascript
 $(document).ready(function() {
 
-//var fr_url = '/data/fr.json';
-//var en_url =  '/data/en.json';
+var fr_url = '/static/data/fr.json';
+var en_url =  '/static/data/en.json';
 var common_url = '/static/data/common.json';
 
-//var fr = $.getJson(fr_url);
-//var en = $.getJson(en_url);
-$.getJSON(common_url, function(data){
-    $('#name').html(data.name.first+' '+data.name.last);
-    $('#email').html(data.email);
-    $('#body').show();
+$('#flag_fr').click(function(){
+    $.getJSON(fr_url, function(data){
+        console.log('fr');
+        $('#contact_button_title').html(data.contact_panel);
+        $('#resume_button_title').html(data.resume_panel);
+    });
 });
+
+$('#contact_panel_content img[name=contact_picture]').click(function() {
+    var src = $('#contact_panel_content img[name=contact_picture]').attr('src');
+    src = src.replace('.png', '_big.png');
+    console.log(src);
+    POPUP.setContent('<img src="'+src+'" width="100%"/>');
+    POPUP.show();
+});
+
+
+$('#flag_en').click(function(){
+    $.getJSON(en_url, function(data){
+        console.log('en');
+        $('#contact_button_title').html(data.contact_panel);
+        $('#resume_button_title').html(data.resume_panel);
+    });
+});
+
+$('#popup .close_button').click(function(){
+    POPUP.hide();
+});
+
+    POPUP = {
+        show: function() {
+            $('#border_container').addClass('overlay');
+            $('#popup').fadeIn();
+        },
+        hide: function() {
+            $('#border_container').removeClass('overlay');
+            $('#popup').slideUp();
+        },
+        setContent: function(content) {
+            $('#popup .content').html(content);
+        }
+    }
 
 });
