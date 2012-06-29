35db4d170f2af7612eb01131e0b00377ad289828
Arthur Rio <arthurio8@hotmail.com>
2012-06-28 23:44:03 -0700

    remove some settings

--- site_perso/settings/__init__.py
+++ site_perso/settings/__init__.py
@@ -112,6 +112,6 @@ INSTALLED_APPS = (
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
-EMAIL_HOST_USER = 'arthur.rio44@gmail.com'
-EMAIL_HOST_PASSWORD = '*-Hockeydreamer5-*'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
